fd57a736788708b601ff5fa0d7ec09118d7a0884
Junsik <chzhgod@gmail.com>
2023-12-26 15:58:33 +0900

    feat: [auth] User Entity를 이용하여 레포지토리 생성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
