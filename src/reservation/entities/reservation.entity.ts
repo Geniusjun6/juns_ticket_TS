6dab5fe93b56b159025706797813d8c0c071a9e9
Junsik <chzhgod@gmail.com>
2024-01-02 19:37:19 +0900

    feat: [reservation/create] 예약 관련 초기 코드 세팅
    1) Entity 및 dto 파일 생성
    - Reservation : User = N:1
    - Reservation : Performance = N:1
    - Reservation : Seat = 1: N
    - DTO의 경우 우선 Seat ID 를 받게 하였음

---
+++ src/reservation/entities/reservation.entity.ts
@@ -0,0 +1,68 @@
+import { IsEnum, IsNumber } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ReservationStatus } from './reservation-status';
+import { User } from 'src/users/entities/user.entity';
+import { Performance } from 'src/performances/entities/performance.entity';
+import { Seat } from 'src/seats/entities/seat.entity';
+
+@Entity()
+export class Reservation {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNumber()
+  @Column({ type: 'int', nullable: false })
+  userId: number;
+
+  @IsNumber()
+  @Column({ type: 'int', nullable: false })
+  performanceId: number;
+
+  @IsEnum(ReservationStatus)
+  @Column({
+    type: 'enum',
+    enum: ReservationStatus,
+    nullable: false,
+    default: ReservationStatus.Reserved,
+  })
+  status: ReservationStatus;
+
+  @IsNumber()
+  @Column({ type: 'int', nullable: false })
+  totalPrice: number;
+
+  @IsNumber()
+  @Column({ type: 'int', nullable: false })
+  numbers: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn({ default: null })
+  updatedAt?: Date;
+
+  @DeleteDateColumn({ default: null })
+  deletedAt?: Date;
+
+  @ManyToOne(() => User, (user) => user.reservation, {
+    onDelete: 'CASCADE',
+  })
+  user: User;
+
+  @ManyToOne(() => Performance, (performance) => performance.reservation, {
+    onDelete: 'CASCADE',
+  })
+  performance: Performance;
+
+  @OneToMany(() => Seat, (seat) => seat.user)
+  seat: Seat[];
+}
