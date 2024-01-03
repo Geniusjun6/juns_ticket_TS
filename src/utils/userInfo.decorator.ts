1c60317d8476061d236bdd07ed8e4303ba53bf7e
Junsik <chzhgod@gmail.com>
2024-01-03 16:40:09 +0900

    feat: [reservation/read] 예약 조회 기능 API 추가
    1) 예약 조회 기능
    - 나의 예약만 조회 가능
    - 예약 조회 시 예약일 기준 내림차순 정렬
    - 타 테이블과 조인하여 필요한 정보들 함께 응답

--- src/utils/userInfo.decorator.ts
+++ src/utils/userInfo.decorator.ts
@@ -3,7 +3,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const UserInfo = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    // console.log('request: ', request); // 먼가 많은 req 정보들이 나오지만 user 정보도 나옴 근데.. password 도 나옴.
+    console.log('request: ', request.user); // 먼가 많은 req 정보들이 나오지만 user 정보도 나옴 근데.. password 도 나옴.
     return request.user ? request.user : null;
   },
 );
